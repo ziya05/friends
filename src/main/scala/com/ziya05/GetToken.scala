9a830c33ee6b254acc60980734e89321564e965e
Arnold Li <lizizheng05@126.com>
2018-05-04 17:36:43 +0800

    add gitignore

--- src/main/scala/com/ziya05/GetToken.scala
+++ src/main/scala/com/ziya05/GetToken.scala
@@ -1,8 +1,19 @@
 package com.ziya05
 
+import org.apache.spark.sql.SparkSession
+
 object GetToken {
   def main(args:Array[String]):Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("GetToken")
+      .getOrCreate()
 
-    
+    val ds = spark.read.textFile("/home/spark/data/friends/*._EN.lrc")
+
+    ds.collect().foreach(println)
+
+    spark.close()
+
   }
 }
