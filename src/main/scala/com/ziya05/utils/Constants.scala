c8257be5544bb456a8ae3e9a3f2f4413db314706
Arnold Li <lizizheng05@126.com>
2018-05-05 17:48:59 +0800

    update pattern

--- src/main/scala/com/ziya05/utils/Constants.scala
+++ src/main/scala/com/ziya05/utils/Constants.scala
@@ -3,7 +3,7 @@ package com.ziya05.utils
 object Constants {
   val SRC_FILE = "hdfs://master:9000/friends/*_EN.lrc"
 
-  val PTN_EPI = """.*(\d+)x(\d+).*"""
+  val PTN_EPI = """.*(\d+)[xX](\d+).*"""
   val PTN_CONTENT = """(\[.*?\])(.*)"""
   val PTN_CTN_SPLIT_CHARS = """[ ,.!?:]"""
 }
