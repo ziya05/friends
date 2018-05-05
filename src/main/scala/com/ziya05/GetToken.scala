b714f08d56fb6f30f2d9850d3efbcf5816204df9
Arnold Li <lizizheng05@126.com>
2018-05-05 09:06:25 +0800

    parse subtitle from file and get episode, season, time and content

--- src/main/scala/com/ziya05/GetToken.scala
+++ src/main/scala/com/ziya05/GetToken.scala
@@ -1,6 +1,8 @@
 package com.ziya05
 
+import com.ziya05.utils.Constants
 import org.apache.spark.sql.SparkSession
+
 
 object GetToken {
   def main(args:Array[String]):Unit = {
@@ -9,7 +11,21 @@ object GetToken {
       .appName("GetToken")
       .getOrCreate()
 
-    val ds = spark.read.textFile("hdfs://master:9000/friends/*_EN.lrc")
+    import spark.implicits._
+    import org.apache.spark.sql.functions.input_file_name
+    val ds = spark.read
+      .text(Constants.SRC_FILE)
+        .select(input_file_name, $"value")
+        .as[(String, String)]
+        .map(fv => {
+          val regEpi = Constants.PTN_EPI.r
+          val regEpi(season, episode) = fv._1
+
+          val regContent = Constants.PTN_CONTENT.r
+          val regContent(time, content) = fv._2
+          (season, episode, time, content.trim)
+        })
+        .filter(x => !x._4.isEmpty)
 
     ds.collect().foreach(println)
 
---
+++ src/main/scala/com/ziya05/utils/Constants.scala
@@ -0,0 +1,8 @@
+package com.ziya05.utils
+
+object Constants {
+  val SRC_FILE = "hdfs://master:9000/friends/*_EN.lrc"
+
+  val PTN_EPI = """.*(\d+)x(\d+).*"""
+  val PTN_CONTENT = """(\[.*?\])(.*)"""
+}
