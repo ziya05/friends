d9b346b5f48950e6e5a19e2277db8723201c8e7b
Arnold Li <lizizheng05@126.com>
2018-05-08 13:15:20 +0800

    generate new subtitle

--- src/main/scala/com/ziya05/GetToken.scala
+++ src/main/scala/com/ziya05/GetToken.scala
@@ -1,8 +1,8 @@
 package com.ziya05
 
 import com.ziya05.utils.Constants
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{input_file_name, count, desc}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions.{count, desc, input_file_name}
 
 object GetToken {
   def main(args:Array[String]):Unit = {
@@ -36,19 +36,42 @@ object GetToken {
     }).filter(x => !x._4.isEmpty)
       .toDF("season", "episode", "time", "word")
 
-    val statByEpiDf = statDf
-        .groupBy("season", "episode", "word")
-        .agg(count("word").alias("quantity"))
-        .select("season", "episode", "word", "quantity")
-        .sort("season", "episode", "quantity")
+//    val statByEpiDf = statDf
+//        .groupBy("season", "episode", "word")
+//        .agg(count("word").alias("quantity"))
+//        .select("season", "episode", "word", "quantity")
+//        .sort("season", "episode", "quantity")
 
-    val statBySeaDf = statDf
+    val statBySeaDs = statDf
         .groupBy("season", "word")
         .agg(count("word").alias("quantity"))
         .select("season", "word", "quantity")
-        .orderBy($"season", desc("quantity"))
+        .orderBy($"season", $"quantity")
+        .filter($"quantity" < 5)
 
-    statBySeaDf.collect().foreach(println)
+    //println(statBySeaDs.schema)
+      .map{
+        case Row(season:String, word:String, quantity:Long) => {
+          (season, word)
+        }
+      }
+      .rdd.groupByKey()
+      .map(x => (x._1, x._2.toSeq))
+      .toDS()
+
+    val result = ds.joinWith(statBySeaDs,
+      ds("_1") === statBySeaDs("_1"))
+        .map(x => {
+          val data = x._1._4.intersect(x._2._2)
+          (x._1._1, x._1._2, x._1._3, data.mkString(" "))
+        })
+
+
+    result
+        .filter(x => x._1 == "1" && x._2 == "01")
+      .collect()
+      .sortBy(x => x._3)
+      .foreach(println)
 
     spark.close()
   }
